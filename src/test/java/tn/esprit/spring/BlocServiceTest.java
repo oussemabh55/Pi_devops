3abc09f7684ac166e227c50cb4bb900a15627a5f
oussema Ben hmaid <oussemabenhmaid98@gmail.com>
2025-07-13 10:47:36 +0100

    Ajout du Jenkinsfile(last)

--- src/test/java/tn/esprit/spring/BlocServiceTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceTest.java
@@ -4,12 +4,16 @@ import org.junit.After;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @TestMethodOrder(MethodOrderer.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 @SpringBootTest
+
+
 public class BlocServiceTest {
 
     @BeforeAll
